79ac7bf2eab0a8fbae28cc1661eb168a492d46e5
Drin Ramadani <38039787+drinramadani-code@users.noreply.github.com>
2021-07-20 20:12:43 +0200

    Delete Portfolio directory

--- Portfolio/dbh.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $conn = mysqli_connect("localhost", "root", "", "portfolio");
-
-  if ($conn -> connect_errno) {
-    echo "Failed to connect to MySQL: " . $conn -> connect_error;
-    exit();
-  }
